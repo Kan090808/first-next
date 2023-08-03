a359d9fadc9162a7c403408e34f60109b7c10574
Jayden <kanjingterng@gmail.com>
2023-08-03 14:32:22 +0800

    first commit

---
+++ my-next-app/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = {
+  assetPrefix: process.env.BASE_PATH || '',
+  basePath: process.env.BASE_PATH || '',
+  // output: 'export',
+}
+
